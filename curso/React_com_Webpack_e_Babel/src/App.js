e436567d864967f7905d71a464cdedc4ae5f9e1a
Eduardo Amroginski Santos <eduardo.amroginski@pampa.compasso>
2020-12-29 11:12:06 -0300

    Adicionando formas de iniciar um projeto em REACT

---
+++ curso/React_com_Webpack_e_Babel/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return <a href="https://www.origamid.com">Origamid</a>;
+};
+export default App;
---
+++ curso/React_com_create-react-app/meuapp/src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Meu App</div>;
+};
+
+export default App;
